b9cbec29f65e4109a29a3b9272569de193dfe770
eddison penagos <eddison_penagos82182@elpoli.edu.co>
2022-02-11 21:57:45 -0500

    git

---
+++ trabajo de pruebasGit/src/main/java/cronometro.java
@@ -0,0 +1,24 @@
+
+public class cronometro {
+
+    public static void main(String[] args) {
+
+        int segundos= 100;
+
+        for (int i = 0; i < segundos; i++) {
+            System.out.println(i+1);
+            delaySegundos();
+        }
+
+
+    }
+
+
+    public static void delaySegundos() {
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e){
+
+        }
+    }
+}
